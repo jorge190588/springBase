42bd854ec5fdbc5218979b932c73482c0c760fb0
jorge190588 <netneill@hotmail.com>
2018-06-26 06:50:47 -0600

    add loginController file

---
+++ student_web/src/com/student/controller/LoginController.java
@@ -0,0 +1,35 @@
+package com.student.controller;
+
+import java.security.Principal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+	@GetMapping
+	public String index(Model model,Principal principal){
+		return "index";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView loginPage(@RequestParam(value = "error",required = false) String error,
+	@RequestParam(value = "logout",	required = false) String logout) {
+		
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Invalid Credentials provided.");
+		}
+
+		if (logout != null) {
+			model.addObject("message", "Logged out from JournalDEV successfully.");
+		}
+
+		model.setViewName("login");
+		return model;
+	}
+}
